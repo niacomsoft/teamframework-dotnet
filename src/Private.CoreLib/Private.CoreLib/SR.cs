9b1708b0a58a095edec8fa9beeaae27dd6317c8f
Wang Yucai <netowls-studio@outlook.com>
2024-01-11 21:47:52 +0800

    refactor(core-lib): 新增 Shared 项目，调增 CoreLib 项目代码到共享项目中。

--- src/Private.CoreLib/Private.CoreLib/SR.cs
+++ src/Private.CoreLib/Private.CoreLib/SR.cs
@@ -1,5 +1,6 @@
 ﻿// © 2024 WANG YUCAI. LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
 
+using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 
@@ -36,5 +37,32 @@ namespace Niacomsoft
         [SuppressMessage("Design", "Ex0100:Member may throw undocumented exception", Justification = "<挂起>")]
         public static string GetString(string name)
             => BuiltInResourceStringResolver.Default.Value.GetString(name);
+
+        /// <summary>
+        /// 使用特定文化区域的资源字符串格式化。
+        /// </summary>
+        /// <param name="name"> 资源名称。 </param>
+        /// <param name="culture">
+        /// 特定的文化区域。
+        /// <para> <see cref="CultureInfo" /> 类型的对象实例。 </para>
+        /// </param>
+        /// <param name="args"> 格式化字符串。 </param>
+        /// <returns> 格式化后的资源字符串。 </returns>
+        /// <seealso cref="CultureInfo" />
+        /// <seealso cref="string.Format(string, object[])" />
+        /// <exception cref="FormatException"> 当调用 <see cref="string.Format(string, object[])" /> 方法时，可能引发此类型的异常。 </exception>
+        public static string Format(string name, CultureInfo culture, params object[] args)
+            => string.Format(GetString(name, culture), args);
+
+        /// <summary>
+        /// 使用特定文化区域的资源字符串格式化。
+        /// </summary>
+        /// <param name="name"> 资源名称。 </param>
+        /// <param name="args"> 格式化字符串。 </param>
+        /// <returns> 格式化后的资源字符串。 </returns>
+        /// <seealso cref="string.Format(string, object[])" />
+        /// <exception cref="FormatException"> 当调用 <see cref="string.Format(string, object[])" /> 方法时，可能引发此类型的异常。 </exception>
+        public static string Format(string name, params object[] args)
+            => string.Format(GetString(name), args);
     }
 }
