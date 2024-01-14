a3b02c27f6095794aa10e2c9da57d6e7bf96e9d4
Wang Yucai <netowls-studio@outlook.com>
2024-01-14 23:10:58 +0800

    feat(core-lib): 新增通用事件参数和委托定义。

---
+++ src/Private.CoreLib/GeneralEventHandler.cs
@@ -0,0 +1,13 @@
+﻿// © 2024 WANG YUCAI. LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
+
+namespace Niacomsoft
+{
+    /// <summary> 定义了处理事件的委托。 </summary>
+    /// <param name="sender"> 触发事件的对象实例。 </param>
+    /// <param name="e">
+    /// 通用事件参数。
+    /// <para> <see cref="GeneralEventArgs" /> 类型的对象实例。 </para>
+    /// </param>
+    /// <seealso cref="GeneralEventArgs" />
+    public delegate void GeneralEventHandler(object sender, GeneralEventArgs e);
+}
---
+++ src/Private.CoreLib/GeneralEventHandler`1.cs
@@ -0,0 +1,14 @@
+﻿// © 2024 WANG YUCAI. LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
+
+namespace Niacomsoft
+{
+    /// <summary> 定义了处理事件的委托。 </summary>
+    /// <typeparam name="TData"> 事件交互数据类型。 </typeparam>
+    /// <param name="sender"> 触发事件的对象实例。 </param>
+    /// <param name="e">
+    /// 通用事件参数。
+    /// <para> <see cref="GeneralEventArgs{TData}" /> 类型的对象实例。 </para>
+    /// </param>
+    /// <seealso cref="GeneralEventArgs{TData}" />
+    public delegate void GeneralEventHandler<TData>(object sender, GeneralEventArgs<TData> e);
+}
