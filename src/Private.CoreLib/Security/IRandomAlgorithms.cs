f58889572bc6e29e71e71fe79d1dc7ba93a7ded7
Wang Yucai <netowls-studio@outlook.com>
2024-01-16 22:55:42 +0800

    feat(core-lib): 新增随机算法接口。

---
+++ src/Private.CoreLib/Security/IRandomAlgorithms.cs
@@ -0,0 +1,16 @@
+﻿// © 2024 WANG YUCAI. LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
+
+namespace Niacomsoft.Security
+{
+    /// <summary> 定义了随机算法的接口。 </summary>
+    public interface IRandomAlgorithms
+    {
+        /// <summary> 最大的随机数种子值。 </summary>
+        /// <value> 获取一个 <see cref="int" /> 类型值，用于表示最大的随机数种子值。 </value>
+        int MaxValue { get; }
+
+        /// <summary> 获取下一个随机值。 </summary>
+        /// <returns> <see cref="int" /> 类型的值。 </returns>
+        int Next();
+    }
+}
