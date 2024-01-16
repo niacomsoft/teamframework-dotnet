a1ffcfaaa01793ca335b5aebad6abd2903813b64
Wang Yucai <netowls-studio@outlook.com>
2024-01-16 22:05:17 +0800

    feat(core-lib): 新增注解辅助方法。

--- src/Private.CoreLib/Utilities/AttributeUtilities.Module.cs
+++ src/Private.CoreLib/Utilities/AttributeUtilities.Module.cs
@@ -8,51 +8,50 @@ using Niacomsoft.Diagnostics;
 
 namespace Niacomsoft.Utilities
 {
-    /// <summary> 提供了注解相关的工具方法。 </summary>
     public static partial class AttributeUtilities
     {
-        /// <summary> 从程序集 <see cref="Assembly" /> 中获取 <paramref name="attributeType" /> 类型的注解。 </summary>
-        /// <param name="assembly">
-        /// 程序集。
-        /// <para> <see cref="Assembly" /> 类型的对象实例。 </para>
+        /// <summary> 从模块 <see cref="Module" /> 中获取 <paramref name="attributeType" /> 类型的注解。 </summary>
+        /// <param name="module">
+        /// 模块。
+        /// <para> <see cref="Module" /> 类型的对象实例。 </para>
         /// </param>
         /// <param name="attributeType"> 派生自 <see cref="Attribute" /> 的类型。 </param>
         /// <param name="inherit"> 是否搜索类型继承链。 </param>
         /// <returns> 派生自 <see cref="Attribute" /> 类型的对象实例。 </returns>
-        /// <seealso cref="Assembly" />
+        /// <seealso cref="Module" />
         /// <seealso cref="Attribute" />
-        /// <seealso cref="Attribute.GetCustomAttribute(Assembly, Type, bool)" />
+        /// <seealso cref="Attribute.GetCustomAttribute(Module, Type, bool)" />
         /// <seealso cref="Type" />
         /// <exception cref="AmbiguousMatchException">
-        /// 当调用 <see cref="Attribute.GetCustomAttribute(Assembly, Type, bool)" /> 方法时，可能引发此类型的异常。
+        /// 当调用 <see cref="Attribute.GetCustomAttribute(Module, Type, bool)" /> 方法时，可能引发此类型的异常。
         /// </exception>
-        public static Attribute GetCustomAttribute(Assembly assembly, Type attributeType, bool inherit = false)
+        public static Attribute GetCustomAttribute(Module module, Type attributeType, bool inherit = false)
         {
-            Guard.ArgumentNull(assembly, nameof(assembly), nameof(GetCustomAttribute));
+            Guard.ArgumentNull(module, nameof(module), nameof(GetCustomAttribute));
             Guard.ArgumentNull(attributeType, nameof(attributeType), nameof(GetCustomAttribute));
-            return Attribute.GetCustomAttribute(assembly, attributeType, inherit);
+            return Attribute.GetCustomAttribute(module, attributeType, inherit);
         }
 
-        /// <summary> 从程序集 <see cref="Assembly" /> 中获取 <typeparamref name="TAttribute" /> 类型的注解。 </summary>
+        /// <summary> 从模块 <see cref="Module" /> 中获取 <typeparamref name="TAttribute" /> 类型的注解。 </summary>
         /// <typeparam name="TAttribute"> 派生自 <see cref="Attribute" /> 的类型。 </typeparam>
-        /// <param name="assembly">
-        /// 程序集。
-        /// <para> <see cref="Assembly" /> 类型的对象实例。 </para>
+        /// <param name="module">
+        /// 模块。
+        /// <para> <see cref="Module" /> 类型的对象实例。 </para>
         /// </param>
         /// <param name="inherit"> 是否搜索类型继承链。 </param>
         /// <returns> <typeparamref name="TAttribute" /> 类型的对象实例。 </returns>
-        /// <seealso cref="GetCustomAttribute(Assembly, Type, bool)" />
+        /// <seealso cref="GetCustomAttribute(Module, Type, bool)" />
         [SuppressMessage("Design", "Ex0100:Member may throw undocumented exception", Justification = "<挂起>")]
-        public static TAttribute GetCustomAttribute<TAttribute>(Assembly assembly, bool inherit = false) where TAttribute : Attribute
-            => GetCustomAttribute(assembly, typeof(TAttribute), inherit) as TAttribute;
+        public static TAttribute GetCustomAttribute<TAttribute>(Module module, bool inherit = false) where TAttribute : Attribute
+            => GetCustomAttribute(module, typeof(TAttribute), inherit) as TAttribute;
 
         /// <summary>
-        /// 当从程序集 <paramref name="assembly" /> 中获取 <paramref name="attributeType" /> 类型的注解不等于 <see langword="null" /> 时，返回
+        /// 当从模块 <paramref name="module" /> 中获取 <paramref name="attributeType" /> 类型的注解不等于 <see langword="null" /> 时，返回
         /// <see langword="true" />；否则返回 <see langword="false" />。
         /// </summary>
-        /// <param name="assembly">
-        /// 程序集。
-        /// <para> <see cref="Assembly" /> 类型的对象实例。 </para>
+        /// <param name="module">
+        /// 模块。
+        /// <para> <see cref="Module" /> 类型的对象实例。 </para>
         /// </param>
         /// <param name="attributeType"> 派生自 <see cref="Attribute" /> 的类型。 </param>
         /// <param name="inherit"> 是否搜索类型继承链。 </param>
@@ -61,18 +60,18 @@ namespace Niacomsoft.Utilities
         /// <para> 派生自 <see cref="Attribute" /> 类型的对象实例。 </para>
         /// </param>
         /// <returns>
-        /// 当从程序集 <paramref name="assembly" /> 中获取 <paramref name="attributeType" /> 类型的注解不等于 <see langword="null" /> 时，返回
+        /// 当从模块 <paramref name="module" /> 中获取 <paramref name="attributeType" /> 类型的注解不等于 <see langword="null" /> 时，返回
         /// <see langword="true" />；否则返回 <see langword="false" />。
         /// </returns>
-        /// <seealso cref="Assembly" />
+        /// <seealso cref="Module" />
         /// <seealso cref="Attribute" />
-        /// <seealso cref="GetCustomAttribute(Assembly, Type, bool)" />
+        /// <seealso cref="GetCustomAttribute(Module, Type, bool)" />
         /// <seealso cref="Type" />
-        public static bool TryGetCustomAttribute(Assembly assembly, Type attributeType, bool inherit, out Attribute attribute)
+        public static bool TryGetCustomAttribute(Module module, Type attributeType, bool inherit, out Attribute attribute)
         {
             try
             {
-                attribute = GetCustomAttribute(assembly, attributeType, inherit);
+                attribute = GetCustomAttribute(module, attributeType, inherit);
                 return AssertUtilities.NotNull(attribute);
             }
             catch (Exception error)
@@ -84,13 +83,13 @@ namespace Niacomsoft.Utilities
         }
 
         /// <summary>
-        /// 当从程序集 <paramref name="assembly" /> 中获取 <typeparamref name="TAttribute" /> 类型的注解不等于 <see langword="null" /> 时，返回
+        /// 当从模块 <paramref name="module" /> 中获取 <typeparamref name="TAttribute" /> 类型的注解不等于 <see langword="null" /> 时，返回
         /// <see langword="true" />；否则返回 <see langword="false" />。
         /// </summary>
         /// <typeparam name="TAttribute"> 派生自 <see cref="Attribute" /> 的类型。 </typeparam>
-        /// <param name="assembly">
-        /// 程序集。
-        /// <para> <see cref="Assembly" /> 类型的对象实例。 </para>
+        /// <param name="module">
+        /// 模块。
+        /// <para> <see cref="Module" /> 类型的对象实例。 </para>
         /// </param>
         /// <param name="inherit"> 是否搜索类型继承链。 </param>
         /// <param name="attribute">
@@ -98,16 +97,16 @@ namespace Niacomsoft.Utilities
         /// <para> 派生自 <see cref="Attribute" /> 类型的对象实例。 </para>
         /// </param>
         /// <returns>
-        /// 当从程序集 <paramref name="assembly" /> 中获取 <typeparamref name="TAttribute" /> 类型的注解不等于 <see langword="null" /> 时，返回
+        /// 当从模块 <paramref name="module" /> 中获取 <typeparamref name="TAttribute" /> 类型的注解不等于 <see langword="null" /> 时，返回
         /// <see langword="true" />；否则返回 <see langword="false" />。
         /// </returns>
-        /// <seealso cref="TryGetCustomAttribute(Assembly, Type, bool, out Attribute)" />
-        /// <seealso cref="Assembly" />
+        /// <seealso cref="TryGetCustomAttribute(Module, Type, bool, out Attribute)" />
+        /// <seealso cref="Module" />
         /// <seealso cref="Attribute" />
-        public static bool TryGetCustomAttribute<TAttribute>(Assembly assembly, bool inherit, out TAttribute attribute) where TAttribute : Attribute
+        public static bool TryGetCustomAttribute<TAttribute>(Module module, bool inherit, out TAttribute attribute) where TAttribute : Attribute
         {
             attribute = default;
-            if (TryGetCustomAttribute(assembly, typeof(TAttribute), inherit, out Attribute attr))
+            if (TryGetCustomAttribute(module, typeof(TAttribute), inherit, out Attribute attr))
             {
                 attribute = attr as TAttribute;
                 return AssertUtilities.NotNull(attribute);
