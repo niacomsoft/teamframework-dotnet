8dabdd9db80d5791bf96608adfe371a87080eeb4
Wang Yucai <netowls-studio@outlook.com>
2024-01-17 21:53:53 +0800

    feat(core-lib): 修改了 IRandomAlgorithms，增加了 Shuffle 等方法定义。

--- src/Private.CoreLib/Security/IRandomAlgorithms.cs
+++ src/Private.CoreLib/Security/IRandomAlgorithms.cs
@@ -9,8 +9,17 @@ namespace Niacomsoft.Security
         /// <value> 获取一个 <see cref="int" /> 类型值，用于表示最大的随机数种子值。 </value>
         int MaxValue { get; }
 
-        /// <summary> 获取下一个随机值。 </summary>
+        /// <summary> 打乱一次顺序。 </summary>
+        /// <returns> 实现了 <see cref="IRandomAlgorithms" /> 类型接口的对象实例。 </returns>
+        IRandomAlgorithms Shuffle();
+
+        /// <summary> 随机获取一个值。 </summary>
         /// <returns> <see cref="int" /> 类型的值。 </returns>
-        int Next();
+        int GetValue();
+
+        /// <summary> 获取指定个数 <paramref name="count" /> 的随机值数组。 </summary>
+        /// <param name="count"> 指定的个数。 </param>
+        /// <returns> 长度为 <paramref name="count" /> 的随机值数组。 </returns>
+        int[] GetValues(int count = 1);
     }
 }
