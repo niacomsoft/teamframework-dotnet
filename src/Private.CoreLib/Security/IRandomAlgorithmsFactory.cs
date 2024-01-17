8a18ff55a74b29616ce1e71ea5bfcc18489ae4c7
Wang Yucai <netowls-studio@outlook.com>
2024-01-17 22:42:22 +0800

    feat(core-lib): 新增随机算法工厂接口和基类。

---
+++ src/Private.CoreLib/Security/IRandomAlgorithmsFactory.cs
@@ -0,0 +1,26 @@
+﻿// © 2024 WANG YUCAI. LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
+
+namespace Niacomsoft.Security
+{
+    /// <summary> 定义了创建随机数算法的工厂方法接口。 </summary>
+    /// <seealso cref="IRandomAlgorithms" />
+    public interface IRandomAlgorithmsFactory
+    {
+        /// <summary> 创建随机数算法。 </summary>
+        /// <param name="maxValue"> 最大的种子值。 </param>
+        /// <returns>
+        /// 随机数算法。
+        /// <para> 实现了 <see cref="IRandomAlgorithms" /> 类型接口的对象实例。 </para>
+        /// </returns>
+        /// <seealso cref="IRandomAlgorithms" />
+        IRandomAlgorithms Create(int maxValue);
+
+        /// <summary> 创建随机数算法。 </summary>
+        /// <returns>
+        /// 随机数算法。
+        /// <para> 实现了 <see cref="IRandomAlgorithms" /> 类型接口的对象实例。 </para>
+        /// </returns>
+        /// <seealso cref="IRandomAlgorithms" />
+        IRandomAlgorithms Create();
+    }
+}
---
+++ src/Private.CoreLib/Security/RandomAlgorithmsFactory.cs
@@ -0,0 +1,19 @@
+﻿// © 2024 WANG YUCAI. LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
+
+namespace Niacomsoft.Security
+{
+    /// <summary> 提供了创建随机数算法相关的工厂方法。 </summary>
+    /// <seealso cref="IRandomAlgorithms" />
+    /// <seealso cref="IRandomAlgorithmsFactory" />
+    /// <seealso cref="RandomAlgorithms" />
+    public class RandomAlgorithmsFactory : IRandomAlgorithmsFactory
+    {
+        /// <inheritdoc />
+        public virtual IRandomAlgorithms Create(int maxValue)
+            => new RandomAlgorithms(maxValue);
+
+        /// <inheritdoc />
+        public virtual IRandomAlgorithms Create()
+            => new RandomAlgorithms();
+    }
+}
