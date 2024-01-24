5f746a8c30efaed25a3f8ef7ac5c88bd3286616a
Wang Yucai <netowls-studio@outlook.com>
2024-01-24 22:57:05 +0800

    feat(core-lib): 新增上下文数据接口和基类。

---
+++ src/Private.CoreLib/ContextualData.cs
@@ -0,0 +1,72 @@
+﻿// © 2024 WANG YUCAI. LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+using Niacomsoft.Utilities;
+
+namespace Niacomsoft
+{
+    /// <summary> 提供了上下文数据相关的方法。 </summary>
+    /// <seealso cref="IContextualData" />
+    public class ContextualData : IContextualData
+    {
+        /// <summary> 初始化 <see cref="ContextualData" /> 类的新实例。 </summary>
+        public ContextualData()
+        {
+            Buffer = new Hashtable();
+        }
+
+        /// <summary> 初始化 <see cref="ContextualData" /> 类的新实例。 </summary>
+        /// <param name="initialValues"> 初始化数据。 </param>
+        public ContextualData(IDictionary<string, object> initialValues) : this()
+        {
+            if (AssertUtilities.NotNull(initialValues))
+            {
+                foreach (var item in initialValues)
+                {
+#pragma warning disable Ex0100 // Member may throw undocumented exception
+                    Buffer[item.Key] = item.Value;
+#pragma warning restore Ex0100 // Member may throw undocumented exception
+                }
+            }
+        }
+
+        /// <summary> 上下文数据缓冲区。 </summary>
+        /// <value> 获取 <see cref="Hashtable" /> 类型的对象实例，用于表示上下文数据缓冲区。 </value>
+        /// <seealso cref="Hashtable" />
+        protected virtual Hashtable Buffer { get; }
+
+        /// <inheritdoc />
+        public void SetValue(string key, object value)
+        {
+            InvalidKey(key);
+#pragma warning disable Ex0100 // Member may throw undocumented exception
+            Buffer[key] = value;
+#pragma warning restore Ex0100 // Member may throw undocumented exception
+        }
+
+        /// <inheritdoc />
+        public bool TryGetValue(string key, out object value)
+        {
+            value = null;
+            return Buffer.ContainsKey(key) && AssertUtilities.NotNull(value = Buffer[key]);
+        }
+
+        /// <summary>
+        /// 当上下文数据名称 <paramref name="key" /> 等于 <see langword="null" />、 <see cref="string.Empty" /> 或空白符时，将引发一个
+        /// <see cref="ArgumentException" /> 类型的异常。
+        /// </summary>
+        /// <param name="key"> 需要校验的上下文数据名称。 </param>
+        [SuppressMessage("Design", "Ex0100:Member may throw undocumented exception", Justification = "<挂起>")]
+        protected virtual void InvalidKey(string key)
+        {
+            if (AssertUtilities.IsEmpty(key, EmptyComparisonOptions.NullOrWhitespace))
+            {
+                throw new ArgumentException(SR.Format("ArgumentException_with_parameter_name", nameof(key)), nameof(key));
+            }
+        }
+    }
+}
---
+++ src/Private.CoreLib/IContextualData.cs
@@ -0,0 +1,22 @@
+﻿// © 2024 WANG YUCAI. LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
+
+namespace Niacomsoft
+{
+    /// <summary> 定义了上下文数据的接口。 </summary>
+    public interface IContextualData
+    {
+        /// <summary> 添加或更新名称为 <paramref name="key" /> 的上下文数据。 </summary>
+        /// <param name="key"> 上下文数据标识名称。 </param>
+        /// <param name="value"> 上下文数据。 </param>
+        void SetValue(string key, object value);
+
+        /// <summary> 尝试获取名称为 <paramref name="key" /> 的上下文数据。 </summary>
+        /// <param name="key"> 上下文数据标识名称。 </param>
+        /// <param name="value"> 上下文数据。 </param>
+        /// <returns>
+        /// 当上下文数据中不包含名称为 <paramref name="key" /> 的上下文数据或 <paramref name="value" /> 等于 <see langword="null" /> 时，返回
+        /// <see langword="false" />；否则返回 <see langword="true" />。
+        /// </returns>
+        bool TryGetValue(string key, out object value);
+    }
+}
