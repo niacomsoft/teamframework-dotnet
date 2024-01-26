e0fea7a3226111026787d5799497656faabcbda9
Wang Yucai <netowls-studio@outlook.com>
2024-01-26 22:42:46 +0800

    feat(core-lib): 新增通用上下文接口和基类。

---
+++ src/Private.CoreLib/GenericContext.cs
@@ -0,0 +1,92 @@
+﻿// © 2024 WANG YUCAI. LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+using Niacomsoft.Diagnostics;
+using Niacomsoft.Utilities;
+
+namespace Niacomsoft
+{
+    /// <summary> 提供了通用的应用程序上下文相关的基本方法。 </summary>
+    /// <seealso cref="IGenericContext" />
+    public class GenericContext : IGenericContext
+    {
+        /// <summary> 初始化 <see cref="IContextualData" /> 类的新实例。 </summary>
+        /// <param name="data">
+        /// 上下文数据。
+        /// <para> 实现了 <see cref="IContextualData" /> 类型接口的对象实例。 </para>
+        /// </param>
+        public GenericContext(IContextualData data)
+        {
+            ContextData = data;
+        }
+
+        /// <summary> 初始化 <see cref="GenericContext" /> 类的新实例。 </summary>
+        /// <seealso cref="ContextualData" />
+        public GenericContext() : this(new ContextualData())
+        {
+        }
+
+        /// <inheritdoc />
+        public virtual IContextualData ContextData { get; }
+
+        /// <inheritdoc />
+        public virtual object this[string key]
+        {
+            get
+            {
+                ThrowIfEmptyKey(key);
+                return TryGetData(key.Trim(), out object value) ? value : null;
+            }
+            set
+            {
+                ThrowIfEmptyKey(key);
+                SetData(key.Trim(), value);
+            }
+        }
+
+        /// <inheritdoc />
+        public virtual void Dispose()
+        {
+            Debugger.WriteLine("The application releases the memory resources occupied by the object instance of the GenericContext type.",
+                               null,
+                               DebuggingLevel.Information);
+        }
+
+        /// <inheritdoc />
+        public virtual object GetData(string key)
+        {
+            ThrowIfEmptyKey(key);
+            return TryGetData(key.Trim(), out object value) ? value : null;
+        }
+
+        /// <inheritdoc />
+        public virtual void SetData(string key, object value)
+        {
+            ThrowIfEmptyKey(key);
+            ContextData.SetValue(key.Trim(), value);
+        }
+
+        /// <inheritdoc />
+        public virtual bool TryGetData(string key, out object value)
+        {
+            ThrowIfEmptyKey(key);
+            return ContextData.TryGetValue(key.Trim(), out value);
+        }
+
+        /// <summary>
+        /// 当 <paramref name="key" /> 等于 <see langword="null" />、 <see cref="string.Empty" /> 或全为空白符时，将引发一个
+        /// <see cref="ArgumentException" /> 类型的异常。
+        /// </summary>
+        /// <param name="key"> 需要校验的标识名称。 </param>
+        [SuppressMessage("Design", "Ex0100:Member may throw undocumented exception", Justification = "<挂起>")]
+        protected virtual void ThrowIfEmptyKey(string key)
+        {
+            if (Debugger.IfWriteLine(AssertUtilities.IsEmpty(key, EmptyComparisonOptions.NullOrWhitespace), null, "Invalid contextual data key.", level: DebuggingLevel.Warning))
+            {
+                throw new ArgumentException(SR.Format("ArgumentException_with_parameter_name", nameof(key)), nameof(key));
+            }
+        }
+    }
+}
---
+++ src/Private.CoreLib/IGenericContext.cs
@@ -0,0 +1,41 @@
+﻿// © 2024 WANG YUCAI. LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
+
+using System;
+
+namespace Niacomsoft
+{
+    /// <summary> 定义了通用的上下文的接口。 </summary>
+    /// <seealso cref="IDisposable" />
+    public interface IGenericContext : IDisposable
+    {
+        /// <summary> 上下文数据。 </summary>
+        /// <value> 获取 <see cref="IContextualData" /> 类型的对象实例，用于表示上下文数据。 </value>
+        /// <seealso cref="IContextualData" />
+        IContextualData ContextData { get; }
+
+        /// <summary> 设置标识名称为 <paramref name="key" /> 的上下文数据。 </summary>
+        /// <param name="key"> 上下文数据标识名称。 </param>
+        /// <param name="value"> 上下文数据。 </param>
+        void SetData(string key, object value);
+
+        /// <summary> 获取标识名称为 <paramref name="key" /> 的上下文数据。 </summary>
+        /// <param name="key"> 标识名称。 </param>
+        /// <returns> 上下文数据。 </returns>
+        object GetData(string key);
+
+        /// <summary>
+        /// 当上下文中包含标识名称为 <paramref name="key" /> 的上下文数据，且数据不等于 <see langword="null" /> 时，返回 <see langword="true" />；否则返回 <see langword="false" />。
+        /// </summary>
+        /// <param name="key"> 标识名称。 </param>
+        /// <param name="value"> 上下文数据。 </param>
+        /// <returns>
+        /// 当上下文中包含标识名称为 <paramref name="key" /> 的上下文数据，且数据不等于 <see langword="null" /> 时，返回 <see langword="true" />；否则返回 <see langword="false" />。
+        /// </returns>
+        bool TryGetData(string key, out object value);
+
+        /// <summary> 上下文数据索引器。 </summary>
+        /// <param name="key"> 标识名称。 </param>
+        /// <returns> 标识名称为 <paramref name="key" /> 的上下文数据。 </returns>
+        object this[string key] { get; set; }
+    }
+}
