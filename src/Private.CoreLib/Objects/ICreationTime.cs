f2b8287202e1ebffb27776d1aa4e8acaedc61dda
Wang Yucai <netowls-studio@outlook.com>
2024-01-27 22:57:44 +0800

    feat(core-lib): ⏳ 新增一组描述对象的属性的接口。
    未完！

---
+++ src/Private.CoreLib/Objects/ICreationTime.cs
@@ -0,0 +1,15 @@
+﻿// © 2024 WANG YUCAI. LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
+
+using System;
+
+namespace Niacomsoft.Objects
+{
+    /// <summary> 定义了描述创建时间的接口。 </summary>
+    public interface ICreationTime
+    {
+        /// <summary> 创建时间。 </summary>
+        /// <value> 设置或获取一个 <see cref="DateTime" /> 类型值，用于表示创建时间。 </value>
+        /// <seealso cref="DateTime" />
+        DateTime CreatedAt { get; set; }
+    }
+}
