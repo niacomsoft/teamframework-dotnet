296592f21301aad0117daf8267cfd48790b8a906
Wang Yucai <netowls-studio@outlook.com>
2024-01-29 16:56:02 +0800

    feat(common): 新增服务解析程序抽象。

---
+++ src/Tfx.Common/ServiceResolverBase.cs
@@ -0,0 +1,19 @@
+﻿// © 2024 WANG YUCAI. LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
+
+using System;
+
+namespace Niacomsoft.TeamFramework
+{
+    /// <summary> 提供了解释指定服务相关的 <see langword="abstract" /> 方法。 </summary>
+    /// <seealso cref="IServiceResolver" />
+    public abstract class ServiceResolverBase : IServiceResolver
+    {
+        /// <summary> 初始化 <see cref="ServiceResolverBase" /> 类的新实例。 </summary>
+        protected ServiceResolverBase()
+        {
+        }
+
+        /// <inheritdoc />
+        public abstract object GetService(Type serviceType);
+    }
+}
