8b7b8cb5f21214a8e035a94ddaf7863b7c6a5bfe
Wang Yucai <netowls-studio@outlook.com>
2024-01-29 21:45:15 +0800

    refactor(common): 💥 调整了 ServiceResolver 类型名。
    BREAKING CHANGE: 将 "ServiceResolverBase" 重命名为 "ServiceResolver"。

--- src/Tfx.Common/ServiceResolver.cs
+++ src/Tfx.Common/ServiceResolver.cs
@@ -6,10 +6,10 @@ namespace Niacomsoft.TeamFramework
 {
     /// <summary> 提供了解释指定服务相关的 <see langword="abstract" /> 方法。 </summary>
     /// <seealso cref="IServiceResolver" />
-    public abstract class ServiceResolverBase : IServiceResolver
+    public abstract class ServiceResolver : IServiceResolver
     {
-        /// <summary> 初始化 <see cref="ServiceResolverBase" /> 类的新实例。 </summary>
-        protected ServiceResolverBase()
+        /// <summary> 初始化 <see cref="ServiceResolver" /> 类的新实例。 </summary>
+        protected ServiceResolver()
         {
         }
 
