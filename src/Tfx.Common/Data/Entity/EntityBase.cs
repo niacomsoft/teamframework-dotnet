fa329b6ae94f3068eaf9d64c0da89e848d2a527f
Wang Yucai <netowls-studio@outlook.com>
2024-01-31 13:54:21 +0800

    feat(Common): 新增数据实体抽象类。

---
+++ src/Tfx.Common/Data/Entity/EntityBase.cs
@@ -0,0 +1,23 @@
+﻿// © 2024 WANG YUCAI. LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
+
+using System;
+
+namespace Niacomsoft.TeamFramework.Data.Entity
+{
+    /// <summary> 提供了数据实体相关的 <see langword="abstract" /> 方法。 </summary>
+    /// <typeparam name="TId"> </typeparam>
+    public abstract class EntityBase<TId> : IEntityBase<TId>
+    {
+        /// <summary> 初始化 <see cref="EntityBase{TId}" /> 类的新实例。 </summary>
+        protected EntityBase()
+        {
+            CreatedAt = DateTime.Now;
+        }
+
+        /// <inheritdoc />
+        public abstract DateTime CreatedAt { get; set; }
+
+        /// <inheritdoc />
+        public abstract TId Id { get; set; }
+    }
+}
---
+++ src/Tfx.Common/Data/Entity/IEntityBase.cs
@@ -0,0 +1,14 @@
+﻿// © 2024 WANG YUCAI. LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
+
+using Niacomsoft.Objects;
+
+namespace Niacomsoft.TeamFramework.Data.Entity
+{
+    /// <summary> 定义了数据实体的基本信息接口。 </summary>
+    /// <typeparam name="TId"> 数据实体唯一标识类型。 </typeparam>
+    /// <seealso cref="IId{T}" />
+    /// <seealso cref="ICreationTime" />
+    public interface IEntityBase<TId> : IId<TId>, ICreationTime
+    {
+    }
+}
