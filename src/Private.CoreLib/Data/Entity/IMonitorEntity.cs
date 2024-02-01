f4f465578d196a56904bac1aa4f00299c813f850
Wang Yucai <netowls-studio@outlook.com>
2024-02-01 10:05:47 +0800

    feat(CoreLib): 新增可监控的数据实体接口和基类。

---
+++ src/Private.CoreLib/Data/Entity/IMonitorEntity.cs
@@ -0,0 +1,18 @@
+﻿// © 2024 WANG YUCAI. LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
+
+using Niacomsoft.Objects;
+
+namespace Niacomsoft.Data.Entity
+{
+    /// <summary> 定义了可追踪的数据实体的接口。 </summary>
+    /// <typeparam name="TId"> 数据实体唯一标识类型。 </typeparam>
+    /// <seealso cref="IEntityBase{TId}" />
+    /// <seealso cref="IModificationTime" />
+    public interface IMonitorEntity<TId> : IEntityBase<TId>, IModificationTime
+    {
+        /// <summary> 更新 <see cref="IModificationTime.ModifiedAt" /> 属性。 </summary>
+        /// <seealso cref="IModificationTime" />
+        /// <seealso cref="IModificationTime.ModifiedAt" />
+        void UpdateModifiedTime();
+    }
+}
---
+++ src/Private.CoreLib/Data/Entity/MonitorEntity.cs
@@ -0,0 +1,52 @@
+﻿// © 2024 WANG YUCAI. LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
+
+using System;
+
+namespace Niacomsoft.Data.Entity
+{
+    /// <summary> 提供了可监控数据提示相关的 <see langword="abstract" /> 方法。 </summary>
+    /// <typeparam name="TId"> 数据实体唯一标识类型。 </typeparam>
+    /// <seealso cref="EntityBase{TId}" />
+    /// <seealso cref="IMonitorEntity{TId}" />
+    public abstract class MonitorEntity<TId> : EntityBase<TId>, IMonitorEntity<TId>
+    {
+        /// <summary> 初始化 <see cref="MonitorEntity{TId}" /> 类的新实例。 </summary>
+        protected MonitorEntity()
+        {
+#if NET45_OR_GREATER || NETSTANDARD || NETCOREAPP || NET
+            ModifiedAt = null;
+#else
+            ModifiedAt = DateTime.MinValue;
+#endif
+        }
+
+#if NET45_OR_GREATER || NETSTANDARD || NETCOREAPP || NET
+
+        /// <inheritdoc />
+        public virtual DateTime? ModifiedAt { get; set; }
+
+#else
+        /// <inheritdoc />
+        public virtual DateTime ModifiedAt { get; set; }
+#endif
+
+        /// <inheritdoc />
+        public virtual DateTime UpdatedAt
+        {
+            get
+            {
+#if NET45_OR_GREATER || NETSTANDARD || NETCOREAPP || NET
+                return ModifiedAt ?? CreatedAt;
+#else
+                return Niacomsoft.Utilities.AssertUtilities.IsBefore(ModifiedAt, CreatedAt) ? CreatedAt : ModifiedAt;
+#endif
+            }
+        }
+
+        /// <inheritdoc />
+        public virtual void UpdateModifiedTime()
+        {
+            ModifiedAt = DateTime.Now;
+        }
+    }
+}
