55f79f410c9149a10a94a568d42eb6f6e1cd6daa
Wang Yucai <netowls-studio@outlook.com>
2024-02-04 10:36:51 +0800

    feat(Common): 新增 IHashingIntegerConverter 工厂方法接口和基类。

---
+++ src/Tfx.Common.IntegerExtensions.HashingId/HashingIntegerConverterFactory.cs
@@ -0,0 +1,21 @@
+﻿// © 2024 WANG YUCAI. LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
+
+namespace Niacomsoft.TeamFramework.Security.Integer
+{
+    /// <summary> 提供了创建 <see cref="IHashingIntegerConverter" /> 类型的对象实例相关的基本方法。 </summary>
+    /// <seealso cref="IHashingIntegerConverterFactory" />
+    public class HashingIntegerConverterFactory : IHashingIntegerConverterFactory
+    {
+        /// <inheritdoc />
+        public virtual IHashingIntegerConverter Create(HashingIntegerOptions options)
+        {
+            return new HashingIntegerConverter(options);
+        }
+
+        /// <inheritdoc />
+        public virtual IHashingIntegerConverter Create()
+        {
+            return Create(HashingIntegerOptions.DefaultOptions);
+        }
+    }
+}
---
+++ src/Tfx.Common.IntegerExtensions.HashingId/IHashingIntegerConverterFactory.cs
@@ -0,0 +1,27 @@
+﻿// © 2024 WANG YUCAI. LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
+
+namespace Niacomsoft.TeamFramework.Security.Integer
+{
+    /// <summary> 定义了创建 <see cref="IHashingIntegerConverter" /> 类型的对象实例的工厂方法接口。 </summary>
+    public interface IHashingIntegerConverterFactory
+    {
+        /// <summary> 创建 <see cref="IHashingIntegerConverter" /> 类型的对象实例。 </summary>
+        /// <param name="options">
+        /// 整型哈希值选项。
+        /// <para> <see cref="HashingIntegerOptions" /> 类型的对象实例。 </para>
+        /// </param>
+        /// <returns> <see cref="IHashingIntegerConverter" /> 类型的对象实例。 </returns>
+        /// <seealso cref="HashingIntegerOptions" />
+        /// <seealso cref="IHashingIntegerConverter" />
+        IHashingIntegerConverter Create(HashingIntegerOptions options);
+
+        /// <summary>
+        /// 使用默认的选项 <see cref="HashingIntegerOptions.DefaultOptions" /> 创建 <see cref="IHashingIntegerConverter" /> 类型的对象实例。
+        /// </summary>
+        /// <returns> <see cref="IHashingIntegerConverter" /> 类型的对象实例。 </returns>
+        /// <seealso cref="HashingIntegerOptions" />
+        /// <seealso cref="HashingIntegerOptions.DefaultOptions" />
+        /// <seealso cref="IHashingIntegerConverter" />
+        IHashingIntegerConverter Create();
+    }
+}
