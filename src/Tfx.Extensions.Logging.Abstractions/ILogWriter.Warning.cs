f6cc52f6ebb31b7c288868ba8803fd8ff080b5f6
Wang Yucai <netowls-studio@outlook.com>
2024-02-20 22:16:45 +0800

    feat(Logging): 完成了运行时日志接口。
    关联的 Commits: 004ceaeb0f7fc5be8638272cd098011b7ddd136d

---
+++ src/Tfx.Extensions.Logging.Abstractions/ILogWriter.Warning.cs
@@ -0,0 +1,57 @@
+﻿// © 2024 WANG YUCAI. LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
+
+using System;
+
+namespace Niacomsoft.TeamFramework.Extensions.Logging
+{
+    public partial interface ILogWriter
+    {
+        /// <summary> 记录运行时警告日志。 </summary>
+        /// <param name="message"> 运行时日志描述信息。 </param>
+        /// <param name="category"> 运行时日志类别。 </param>
+        /// <seealso cref="LogLevel" />
+        /// <seealso cref="LogLevel.Trace" />
+        void LogWarning(string message, string category = null);
+
+        /// <summary> 记录运行时警告日志。 </summary>
+        /// <param name="message"> 运行时日志描述信息。 </param>
+        /// <param name="category">
+        /// 标识运行时日志类别的类型。
+        /// <para> <see cref="Type" /> 类型的对象实例。 </para>
+        /// </param>
+        /// <seealso cref="LogLevel" />
+        /// <seealso cref="LogLevel.Trace" />
+        /// <seealso cref="Type" />
+        void LogWarning(string message, Type category = null);
+
+        /// <summary> 记录运行时警告日志。 </summary>
+        /// <typeparam name="TCategory"> 标识运行时日志类别的类型。 </typeparam>
+        /// <param name="message"> 运行时日志描述信息。 </param>
+        /// <seealso cref="LogLevel" />
+        /// <seealso cref="LogLevel.Trace" />
+        void LogWarning<TCategory>(string message);
+
+        /// <summary> 记录运行时警告级别异常日志。 </summary>
+        /// <param name="error">
+        /// 运行时异常。
+        /// <para> <see cref="Exception" /> 类型的对象实例。 </para>
+        /// </param>
+        /// <param name="cause"> 引发运行时异常的原因。 </param>
+        /// <seealso cref="Exception" />
+        /// <seealso cref="LogLevel" />
+        /// <seealso cref="LogLevel.Trace" />
+        void LogWarning(Exception error, string cause = null);
+
+        /// <summary> 记录运行时警告级别异常日志。 </summary>
+        /// <typeparam name="TCategory"> 标识运行时日志类别的类型。 </typeparam>
+        /// <param name="error">
+        /// 运行时异常。
+        /// <para> <see cref="Exception" /> 类型的对象实例。 </para>
+        /// </param>
+        /// <param name="cause"> 引发运行时异常的原因。 </param>
+        /// <seealso cref="Exception" />
+        /// <seealso cref="LogLevel" />
+        /// <seealso cref="LogLevel.Trace" />
+        void LogWarning<TCategory>(Exception error, string cause = null);
+    }
+}
--- src/Tfx.Extensions.Logging.Abstractions/ILogWriter.cs
+++ src/Tfx.Extensions.Logging.Abstractions/ILogWriter.cs
@@ -5,7 +5,7 @@ using System;
 namespace Niacomsoft.TeamFramework.Extensions.Logging
 {
     /// <summary> 定义了记录运行时日志的接口。 </summary>
-    public interface ILogWriter
+    public partial interface ILogWriter
     {
         /// <summary> 记录运行时日志。 </summary>
         /// <param name="entry">
@@ -14,6 +14,15 @@ namespace Niacomsoft.TeamFramework.Extensions.Logging
         /// </param>
         /// <seealso cref="LogEntry" />
         void Write(LogEntry entry);
+
+        /// <summary> 记录运行时日志。 </summary>
+        /// <typeparam name="TCategory"> 标识运行时日志的类别。 </typeparam>
+        /// <param name="entry">
+        /// 运行时日志项。
+        /// <para> <see cref="LogEntry{TCategory}" /> 类型的对象实例。 </para>
+        /// </param>
+        /// <seealso cref="LogEntry{TCategory}" />
+        void Write<TCategory>(LogEntry<TCategory> entry);
 
         /// <summary> 记录运行时日志。 </summary>
         /// <param name="message"> 运行时日志描述信息。 </param>
@@ -49,13 +58,33 @@ namespace Niacomsoft.TeamFramework.Extensions.Logging
         /// <seealso cref="LogLevel" />
         void Write<TCategory>(string message, LogLevel level = LogLevel.Default);
 
-        /// <summary> 记录运行时日志。 </summary>
-        /// <typeparam name="TCategory"> 标识运行时日志的类别。 </typeparam>
-        /// <param name="entry">
-        /// 运行时日志项。
-        /// <para> <see cref="LogEntry{TCategory}" /> 类型的对象实例。 </para>
+        /// <summary> 记录运行时异常日志。 </summary>
+        /// <param name="error">
+        /// 运行时异常。
+        /// <para> <see cref="Exception" /> 类型的对象实例。 </para>
         /// </param>
-        /// <seealso cref="LogEntry{TCategory}" />
-        void Write<TCategory>(LogEntry<TCategory> entry);
+        /// <param name="cause"> 引发运行时异常的原因。 </param>
+        /// <param name="level">
+        /// 运行时异常级别。
+        /// <para> <see cref="LogLevel" /> 中的一个值。 </para>
+        /// </param>
+        /// <seealso cref="Exception" />
+        /// <seealso cref="LogLevel" />
+        void Write(Exception error, string cause = null, LogLevel level = LogLevel.Fatal);
+
+        /// <summary> 记录运行时异常日志。 </summary>
+        /// <typeparam name="TCategory"> 标识运行时日志类别的类型。 </typeparam>
+        /// <param name="error">
+        /// 运行时异常。
+        /// <para> <see cref="Exception" /> 类型的对象实例。 </para>
+        /// </param>
+        /// <param name="cause"> 引发运行时异常的原因。 </param>
+        /// <param name="level">
+        /// 运行时异常级别。
+        /// <para> <see cref="LogLevel" /> 中的一个值。 </para>
+        /// </param>
+        /// <seealso cref="Exception" />
+        /// <seealso cref="LogLevel" />
+        void Write<TCategory>(Exception error, string cause = null, LogLevel level = LogLevel.Fatal);
     }
 }
