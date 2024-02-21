7afda8837f67f7ff42958cc08df77451cafdbaee
Wang Yucai <netowls-studio@outlook.com>
2024-02-21 22:37:40 +0800

    chore: 资源代码变更。

--- src/Private.CoreLib/Resources/Strings.Designer.cs
+++ src/Private.CoreLib/Resources/Strings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Niacomsoft.Resources {
+namespace Niacomsoft....Private.CoreLib.Resources {
     using System;
     
     
