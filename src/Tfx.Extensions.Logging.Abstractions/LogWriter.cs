f0851751c6de5c00e4cee3b109ef24f17330e69a
Wang Yucai <netowls-studio@outlook.com>
2024-02-21 22:15:29 +0800

    feat(Logging): 完成了 LogWriter 抽象类。
    关联的 Commits 43ba08fefc828aa5567bca2e5be72264272333e0

--- src/Tfx.Extensions.Logging.Abstractions/LogWriter.cs
+++ src/Tfx.Extensions.Logging.Abstractions/LogWriter.cs
@@ -14,94 +14,184 @@ namespace Niacomsoft.TeamFramework.Extensions.Logging
         }
 
         /// <inheritdoc />
-        public abstract void LogDebug(string message, string category = null);
+        public virtual void LogDebug(string message, string category = null)
+        {
+            Write(message, category, LogLevel.Debug);
+        }
 
         /// <inheritdoc />
-        public abstract void LogDebug(string message, Type category = null);
+        public virtual void LogDebug(string message, Type category = null)
+        {
+            Write(message, category, LogLevel.Debug);
+        }
 
         /// <inheritdoc />
-        public abstract void LogDebug<TCategory>(string message);
+        public virtual void LogDebug<TCategory>(string message)
+        {
+            Write<TCategory>(message, LogLevel.Debug);
+        }
 
         /// <inheritdoc />
-        public abstract void LogDebug(Exception error, string cause = null);
+        public virtual void LogDebug(Exception error, string cause = null)
+        {
+            Write(error, cause, LogLevel.Debug);
+        }
 
         /// <inheritdoc />
-        public abstract void LogDebug<TCategory>(Exception error, string cause = null);
+        public virtual void LogDebug<TCategory>(Exception error, string cause = null)
+        {
+            Write<TCategory>(error, cause, LogLevel.Debug);
+        }
 
         /// <inheritdoc />
-        public abstract void LogError(string message, string category = null);
+        public virtual void LogError(string message, string category = null)
+        {
+            Write(message, category, LogLevel.Error);
+        }
 
         /// <inheritdoc />
-        public abstract void LogError(string message, Type category = null);
+        public virtual void LogError(string message, Type category = null)
+        {
+            Write(message, category, LogLevel.Error);
+        }
 
         /// <inheritdoc />
-        public abstract void LogError<TCategory>(string message);
+        public virtual void LogError<TCategory>(string message)
+        {
+            Write<TCategory>(message, LogLevel.Error);
+        }
 
         /// <inheritdoc />
-        public abstract void LogError(Exception error, string cause = null);
+        public virtual void LogError(Exception error, string cause = null)
+        {
+            Write(error, cause, LogLevel.Error);
+        }
 
         /// <inheritdoc />
-        public abstract void LogError<TCategory>(Exception error, string cause = null);
+        public virtual void LogError<TCategory>(Exception error, string cause = null)
+        {
+            Write<TCategory>(error, cause, LogLevel.Error);
+        }
 
         /// <inheritdoc />
-        public abstract void LogFatal(string message, string category = null);
+        public virtual void LogFatal(string message, string category = null)
+        {
+            Write(message, category, LogLevel.Fatal);
+        }
 
         /// <inheritdoc />
-        public abstract void LogFatal(string message, Type category = null);
+        public virtual void LogFatal(string message, Type category = null)
+        {
+            Write(message, category, LogLevel.Fatal);
+        }
 
         /// <inheritdoc />
-        public abstract void LogFatal<TCategory>(string message);
+        public virtual void LogFatal<TCategory>(string message)
+        {
+            Write<TCategory>(message, LogLevel.Fatal);
+        }
 
         /// <inheritdoc />
-        public abstract void LogFatal(Exception error, string cause = null);
+        public virtual void LogFatal(Exception error, string cause = null)
+        {
+            Write(error, cause, LogLevel.Fatal);
+        }
 
         /// <inheritdoc />
-        public abstract void LogFatal<TCategory>(Exception error, string cause = null);
+        public virtual void LogFatal<TCategory>(Exception error, string cause = null)
+        {
+            Write<TCategory>(error, cause, LogLevel.Fatal);
+        }
 
         /// <inheritdoc />
-        public abstract void LogInformation(string message, string category = null);
+        public virtual void LogInformation(string message, string category = null)
+        {
+            Write(message, category, LogLevel.Information);
+        }
 
         /// <inheritdoc />
-        public abstract void LogInformation(string message, Type category = null);
+        public virtual void LogInformation(string message, Type category = null)
+        {
+            Write(message, category, LogLevel.Information);
+        }
 
         /// <inheritdoc />
-        public abstract void LogInformation<TCategory>(string message);
+        public virtual void LogInformation<TCategory>(string message)
+        {
+            Write<TCategory>(message, LogLevel.Information);
+        }
 
         /// <inheritdoc />
-        public abstract void LogInformation(Exception error, string cause = null);
+        public virtual void LogInformation(Exception error, string cause = null)
+        {
+            Write(error, cause, LogLevel.Information);
+        }
 
         /// <inheritdoc />
-        public abstract void LogInformation<TCategory>(Exception error, string cause = null);
+        public virtual void LogInformation<TCategory>(Exception error, string cause = null)
+        {
+            Write<TCategory>(error, cause, LogLevel.Information);
+        }
 
         /// <inheritdoc />
-        public abstract void LogTrace(string message, string category = null);
+        public virtual void LogTrace(string message, string category = null)
+        {
+            Write(message, category, LogLevel.Trace);
+        }
 
         /// <inheritdoc />
-        public abstract void LogTrace(string message, Type category = null);
+        public virtual void LogTrace(string message, Type category = null)
+        {
+            Write(message, category, LogLevel.Trace);
+        }
 
         /// <inheritdoc />
-        public abstract void LogTrace<TCategory>(string message);
+        public virtual void LogTrace<TCategory>(string message)
+        {
+            Write<TCategory>(message, LogLevel.Trace);
+        }
 
         /// <inheritdoc />
-        public abstract void LogTrace(Exception error, string cause = null);
+        public virtual void LogTrace(Exception error, string cause = null)
+        {
+            Write(error, cause, LogLevel.Trace);
+        }
 
         /// <inheritdoc />
-        public abstract void LogTrace<TCategory>(Exception error, string cause = null);
+        public virtual void LogTrace<TCategory>(Exception error, string cause = null)
+        {
+            Write<TCategory>(error, cause, LogLevel.Trace);
+        }
 
         /// <inheritdoc />
-        public abstract void LogWarning(string message, string category = null);
+        public virtual void LogWarning(string message, string category = null)
+        {
+            Write(message, category, LogLevel.Warning);
+        }
 
         /// <inheritdoc />
-        public abstract void LogWarning(string message, Type category = null);
+        public virtual void LogWarning(string message, Type category = null)
+        {
+            Write(message, category, LogLevel.Warning);
+        }
 
         /// <inheritdoc />
-        public abstract void LogWarning<TCategory>(string message);
+        public virtual void LogWarning<TCategory>(string message)
+        {
+            Write<TCategory>(message, LogLevel.Warning);
+        }
 
         /// <inheritdoc />
-        public abstract void LogWarning(Exception error, string cause = null);
+        public virtual void LogWarning(Exception error, string cause = null)
+        {
+            Write(error, cause, LogLevel.Warning);
+        }
 
         /// <inheritdoc />
-        public abstract void LogWarning<TCategory>(Exception error, string cause = null);
+        public virtual void LogWarning<TCategory>(Exception error, string cause = null)
+        {
+            Write<TCategory>(error, cause, LogLevel.Warning);
+        }
 
         /// <inheritdoc />
         public abstract void Write(LogEntry entry);
@@ -113,19 +203,19 @@ namespace Niacomsoft.TeamFramework.Extensions.Logging
         }
 
         /// <inheritdoc />
-        public virtual void Write(string message, string category = null, LogLevel level = LogLevel.Debug)
+        public virtual void Write(string message, string category = null, LogLevel level = LogLevel.Default)
         {
             Write(new LogEntry(message, level, category));
         }
 
         /// <inheritdoc />
-        public virtual void Write(string message, Type category = null, LogLevel level = LogLevel.Debug)
+        public virtual void Write(string message, Type category = null, LogLevel level = LogLevel.Default)
         {
             Write(new LogEntry(message, category, level));
         }
 
         /// <inheritdoc />
-        public virtual void Write<TCategory>(string message, LogLevel level = LogLevel.Debug)
+        public virtual void Write<TCategory>(string message, LogLevel level = LogLevel.Default)
         {
             Write(new LogEntry<TCategory>(message, level));
         }
