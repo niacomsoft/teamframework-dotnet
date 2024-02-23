be1ebbe430a4871e9a14dce8cf39dd638784951e
Wang Yucai <netowls-studio@outlook.com>
2024-02-23 12:44:06 +0800

    feat(Logging): 新增运行时日志方法工厂类。

---
+++ src/Tfx.Extensions.Logging.NLog/INLogWriterFactory.cs
@@ -0,0 +1,19 @@
+﻿// © 2024 WANG YUCAI. LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
+
+using NLog;
+
+namespace Niacomsoft.TeamFramework.Extensions.Logging
+{
+    /// <summary> 定义了创建基于 <see cref="NLog" /> 记录运行时日志的方法接口。 </summary>
+    /// <seealso cref="ILogWriterFactory" />
+    /// <seealso cref="NLogWriter" />
+    public interface INLogWriterFactory : ILogWriterFactory
+    {
+        /// <summary> 使用 <see cref="Logger" /> 类型的对象实例创建运行时日志记录器实例。 </summary>
+        /// <param name="logger"> <see cref="Logger" /> 类型的对象实例。 </param>
+        /// <returns> <see cref="NLogWriter" /> 类型的对象实例。 </returns>
+        /// <seealso cref="Logger" />
+        /// <seealso cref="NLogWriter" />
+        NLogWriter Create(Logger logger);
+    }
+}
---
+++ src/Tfx.Extensions.Logging.NLog/NLogWriterFactory.cs
@@ -0,0 +1,27 @@
+﻿// © 2024 WANG YUCAI. LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
+
+using Niacomsoft.Utilities;
+
+using NLog;
+
+namespace Niacomsoft.TeamFramework.Extensions.Logging
+{
+    /// <summary> 提供了创建基于 <see cref="NLog" /> 记录运行时日志相关的工厂方法。 </summary>
+    /// <seealso cref="INLogWriterFactory" />
+    /// <seealso cref="LogWriterFactory" />
+    public class NLogWriterFactory : LogWriterFactory, INLogWriterFactory
+    {
+        /// <inheritdoc />
+        /// <seealso cref="NLogWriter" />
+        public override ILogWriter Create()
+        {
+            return new NLogWriter();
+        }
+
+        /// <inheritdoc />
+        public virtual NLogWriter Create(Logger logger)
+        {
+            return AssertUtilities.IsNull(logger) ? new NLogWriter() : new NLogWriter(logger);
+        }
+    }
+}
