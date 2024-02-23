355b6a52fb1ec02bec96f5f6be8ca123b3349fb3
Wang Yucai <netowls-studio@outlook.com>
2024-02-23 17:25:58 +0800

    feat(IO): 新增 FileWatchers 抽象项目。
    Issues #10

---
+++ src/Tfx.Extensions.IO.FileWatchers.Abstractions/FileInfoToken.cs
@@ -0,0 +1,12 @@
+﻿// © 2024 WANG YUCAI. LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
+
+using System;
+
+namespace Niacomsoft.IO
+{
+    /// <summary> 提供了文件信息令牌相关的方法。 </summary>
+    [Serializable]
+    public class FileInfoToken
+    {
+    }
+}
---
+++ src/Tfx.Extensions.IO.FileWatchers.Abstractions/README.md
@@ -0,0 +1,5 @@
+# README
+
+>   This library provides a file system monitor abstraction.
+>
+>   此类库提供了文件系统监控程序抽象。
