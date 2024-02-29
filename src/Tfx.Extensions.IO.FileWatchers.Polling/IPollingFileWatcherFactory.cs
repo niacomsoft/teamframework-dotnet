a90eb0cb28f2ccd3f824ff2294243f07d56a0299
Wang Yucai <netowls-studio@outlook.com>
2024-02-29 22:37:28 +0800

    feat(IO): 新增 PollingFileWatcher 的工厂方法接口和实现。

---
+++ src/Tfx.Extensions.IO.FileWatchers.Polling/IPollingFileWatcherFactory.cs
@@ -0,0 +1,28 @@
+﻿// © 2024 WANG YUCAI. LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
+
+using System;
+using System.IO;
+
+namespace Niacomsoft.TeamFramework.Extensions.IO.FileWatchers
+{
+    /// <summary> 定义了创建 <see cref="IPollingFileWatcher" /> 类型的对象实例的工厂方法接口。 </summary>
+    /// <seealso cref="IFileWatcherFactory" />
+    /// <seealso cref="IPollingFileWatcher" />
+    public interface IPollingFileWatcherFactory : IFileWatcherFactory
+    {
+        /// <summary> 创建轮询式文件监控程序。 </summary>
+        /// <param name="target">
+        /// 需要监控的目标文件信息。
+        /// <para> <see cref="FileInfo" /> 类型的对象实例。 </para>
+        /// </param>
+        /// <param name="interval">
+        /// 轮询一次的时间间隔。
+        /// <para> <see cref="TimeSpan" /> 类型的值。 </para>
+        /// </param>
+        /// <returns> 实现了 <see cref="IPollingFileWatcher" /> 类型接口的对象实例。 </returns>
+        /// <seealso cref="FileInfo" />
+        /// <seealso cref="IPollingFileWatcher" />
+        /// <seealso cref="TimeSpan" />
+        IPollingFileWatcher CreateWatcher(FileInfo target, TimeSpan interval);
+    }
+}
---
+++ src/Tfx.Extensions.IO.FileWatchers.Polling/PollingFileWatcherFactory.cs
@@ -0,0 +1,38 @@
+﻿// © 2024 WANG YUCAI. LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
+
+using System;
+using System.IO;
+
+using Niacomsoft.TeamFramework.Extensions.Logging;
+
+namespace Niacomsoft.TeamFramework.Extensions.IO.FileWatchers
+{
+    /// <summary> 提供了创建轮询式文件监控程序相关的工厂方法。 </summary>
+    /// <seealso cref="FileWatcherFactory" />
+    /// <seealso cref="IPollingFileWatcherFactory" />
+    /// <seealso cref="PollingFileWatcher" />
+    public class PollingFileWatcherFactory : FileWatcherFactory, IPollingFileWatcherFactory
+    {
+        /// <summary> 初始化 <see cref="PollingFileWatcherFactory" /> 类的新实例。 </summary>
+        /// <param name="loggerFactory">
+        /// 创建记录运行时日志方法的工厂。
+        /// <para> 实现了 <see cref="ILogWriterFactory" /> 类型接口的对象实例。 </para>
+        /// </param>
+        public PollingFileWatcherFactory(ILogWriterFactory loggerFactory) : base(loggerFactory)
+        {
+        }
+
+        /// <inheritdoc />
+        public virtual IPollingFileWatcher CreateWatcher(FileInfo target, TimeSpan interval)
+        {
+            return new PollingFileWatcher(target, interval, LoggerFactory.CreateWriter());
+        }
+
+        /// <inheritdoc />
+        /// <seealso cref="PollingFileWatcher" />
+        protected override FileWatcher InternalCreateWatcher(FileInfo target, ILogWriter logWriter)
+        {
+            return new PollingFileWatcher(target, logWriter);
+        }
+    }
+}
