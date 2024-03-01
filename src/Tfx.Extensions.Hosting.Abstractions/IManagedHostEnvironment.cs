4194a063b5a6cec5dca1a1ea2f00a760fa018c99
Wang Yucai <netowls-studio@outlook.com>
2024-03-01 22:32:50 +0800

    feat(HostingEnvironment): 新增托管主机环境信息接口和抽象类。

---
+++ src/Tfx.Extensions.Hosting.Abstractions/IManagedHostEnvironment.cs
@@ -0,0 +1,22 @@
+﻿// © 2024 WANG YUCAI. LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
+
+namespace Niacomsoft.TeamFramework.Extensions.Hosting
+{
+    /// <summary> 定义了 .NET 应用程序托管主机环境信息接口。 </summary>
+    public interface IManagedHostEnvironment
+    {
+        /// <summary> 托管主机环境名称。 </summary>
+        /// <value> 获取一个字符串，用于表示托管主机环境名称。 </value>
+        string EnvironmentName { get; }
+
+        /// <summary>
+        /// 当当前的托管主机环境名称 <see cref="EnvironmentName" /> 与 <paramref name="name" /> 相等时，返回 <see langword="true" />；否则返回 <see langword="false" />。
+        /// </summary>
+        /// <param name="name"> 指定的环境名称。 </param>
+        /// <returns>
+        /// 当当前的托管主机环境名称 <see cref="EnvironmentName" /> 与 <paramref name="name" /> 相等时，返回 <see langword="true" />；否则返回 <see langword="false" />。
+        /// </returns>
+        /// <seealso cref="EnvironmentName" />
+        bool IsSpecifiedEnvironment(string name);
+    }
+}
---
+++ src/Tfx.Extensions.Hosting.Abstractions/ManagedHostEnvironment.cs
@@ -0,0 +1,45 @@
+﻿// © 2024 WANG YUCAI. LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
+
+using System;
+
+using Niacomsoft.Utilities;
+
+namespace Niacomsoft.TeamFramework.Extensions.Hosting
+{
+    /// <summary> 提供了访问 .NET 应用程序托管主机环境信息相关的 <see langword="abstract" /> 方法。 </summary>
+    /// <seealso cref="IManagedHostEnvironment" />
+    public abstract class ManagedHostEnvironment : IManagedHostEnvironment
+    {
+#if DEBUG
+
+        /// <summary>
+        /// 默认的托管主机环境名称。
+        /// <para> 代表开发环境。 </para>
+        /// </summary>
+        private const string DefaultEnvironmentName = "Development";
+
+#else
+        /// <summary>
+        /// 默认的托管主机环境名称。
+        /// <para> 代表生产环境。 </para>
+        /// </summary>
+        private const string DefaultEnvironmentName = "Production";
+#endif
+
+        /// <summary> 初始化 <see cref="ManagedHostEnvironment" /> 类的新实例。 </summary>
+        /// <param name="name"> 托管主机环境名称。 </param>
+        protected ManagedHostEnvironment(string name)
+        {
+            EnvironmentName = StringUtilities.IfEmpty(name, DefaultEnvironmentName, EmptyComparisonOptions.NullOrWhitespace);
+        }
+
+        /// <inheritdoc />
+        public virtual string EnvironmentName { get; }
+
+        /// <inheritdoc />
+        public virtual bool IsSpecifiedEnvironment(string name)
+        {
+            return !AssertUtilities.IsEmpty(name, EmptyComparisonOptions.NullOrWhitespace) && EnvironmentName.Equals(name.Trim(), StringComparison.CurrentCultureIgnoreCase);
+        }
+    }
+}
