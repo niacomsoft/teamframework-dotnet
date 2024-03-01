6bd90c664848c4f67b1ea774bfabb4e83c4a457b
Wang Yucai <netowls-studio@outlook.com>
2024-03-01 14:19:46 +0800

    feat(IO): 新增 Polling-FileWatcher Unity 扩展方法。
    Issues #10

---
+++ src/Tfx.Extensions.IO.FileWatchers.Polling.DI.UnityContainer/PollingUnityContainerExtensions.cs
@@ -0,0 +1,24 @@
+﻿// © 2024 WANG YUCAI. LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
+
+using Niacomsoft.TeamFramework.Extensions.IO.FileWatchers;
+
+using Unity;
+
+namespace Niacomsoft.TeamFramework.Extensions.DependencyInjection
+{
+    /// <summary> 为 <see cref="IUnityContainer" /> 类型提供的扩展方法。 </summary>
+    public static class PollingUnityContainerExtensions
+    {
+        /// <summary> 注册 <see cref="IPollingFileWatcherFactory" /> 服务。 </summary>
+        /// <param name="this"> 实现了 <see cref="IUnityContainer" /> 类型接口的对象实例。 </param>
+        /// <returns> 实现了 <see cref="IUnityContainer" /> 类型接口的对象实例 <paramref name="this" />。 </returns>
+        /// <seealso cref="IFileWatcherFactory" />
+        /// <seealso cref="IPollingFileWatcherFactory" />
+        /// <seealso cref="PollingFileWatcherFactory" />
+        public static IUnityContainer AddPollingFileWatcher(this IUnityContainer @this)
+        {
+            return @this.RegisterSingleton<IFileWatcherFactory, PollingFileWatcherFactory>()
+                        .RegisterSingleton<IPollingFileWatcherFactory, PollingFileWatcherFactory>();
+        }
+    }
+}
