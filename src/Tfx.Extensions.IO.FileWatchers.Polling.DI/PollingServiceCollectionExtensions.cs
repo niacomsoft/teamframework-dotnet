07adfbd8dcfa296ea77df759c9f0cb7c0026854d
Wang Yucai <netowls-studio@outlook.com>
2024-03-01 13:33:40 +0800

    feat(IO): 新增 Polling-FileWatcher 依赖注入容器扩展。

---
+++ src/Tfx.Extensions.IO.FileWatchers.Polling.DI/PollingServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿// © 2024 WANG YUCAI. LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
+
+using Microsoft.Extensions.DependencyInjection;
+
+using Niacomsoft.TeamFramework.Extensions.IO.FileWatchers;
+
+namespace Niacomsoft.TeamFramework.Extensions.DependencyInjection
+{
+    /// <summary> 为 <see cref="IServiceCollection" /> 类型提供的扩展方法。 </summary>
+    public static class PollingServiceCollectionExtensions
+    {
+        /// <summary> 注册 <see cref="IPollingFileWatcherFactory" /> 服务。 </summary>
+        /// <param name="this"> 实现了 <see cref="IServiceCollection" /> 类型接口的对象实例。 </param>
+        /// <returns> 实现了 <see cref="IServiceCollection" /> 类型接口的对象实例 <paramref name="this" />。 </returns>
+        /// <seealso cref="IFileWatcherFactory" />
+        /// <seealso cref="IPollingFileWatcherFactory" />
+        /// <seealso cref="PollingFileWatcherFactory" />
+        public static IServiceCollection AddPollingFileWatcher(this IServiceCollection @this)
+        {
+            return @this.AddSingleton<IFileWatcherFactory, PollingFileWatcherFactory>()
+                        .AddSingleton<IPollingFileWatcherFactory, PollingFileWatcherFactory>();
+        }
+    }
+}
---
+++ src/Tfx.Extensions.IO.FileWatchers.Polling.DI/README.md
@@ -0,0 +1,5 @@
+# README
+
+>   This library provides a clock scheduler-based, polling file monitor.
+>
+>   此类库提供了基于时钟调度器的，轮询文件监控程序。
