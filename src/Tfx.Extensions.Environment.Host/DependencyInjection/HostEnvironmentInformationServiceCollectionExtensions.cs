37659ca403766a69bf7763eb7189805c9ad32276
Wang Yucai <netowls-studio@outlook.com>
2024-03-01 21:47:14 +0800

    feat(HostingEnvironment): 新增 HostEnvironmentInformation 依赖注入扩展方法。

---
+++ src/Tfx.Extensions.Environment.Host/DependencyInjection/HostEnvironmentInformationServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿// © 2024 WANG YUCAI. LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
+
+using Microsoft.Extensions.DependencyInjection;
+
+using Niacomsoft.TeamFramework.Extensions.Configuration.Environment;
+
+namespace Niacomsoft.TeamFramework.Extensions.DependencyInjection
+{
+    /// <summary> 为 <see cref="IServiceCollection" /> 类型提供的扩展方法。 </summary>
+    public static class HostEnvironmentInformationServiceCollectionExtensions
+    {
+        /// <summary> 注册 <see cref="IEnvironmentInformation" /> 服务。 </summary>
+        /// <param name="this"> 实现了 <see cref="IServiceCollection" /> 类型接口的对象实例。 </param>
+        /// <returns> 实现了 <see cref="IServiceCollection" /> 类型接口的对象实例 <paramref name="this" />。 </returns>
+        /// <seealso cref="IEnvironmentInformation" />
+        /// <seealso cref="HostEnvironmentInformation" />
+        public static IServiceCollection AddEnvironmentInformation(this IServiceCollection @this)
+        {
+            return @this.AddSingleton<IEnvironmentInformation, HostEnvironmentInformation>();
+        }
+    }
+}
