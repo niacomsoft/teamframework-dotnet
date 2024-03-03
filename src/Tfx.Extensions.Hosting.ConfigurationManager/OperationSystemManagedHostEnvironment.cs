2b0f4d1f2318262ad1c4874e3b12fbd49c965ee7
Wang Yucai <netowls-studio@outlook.com>
2024-03-03 22:38:34 +0800

    feat(HostingEnvironment): 新增基于系统环境变量的托管环境服务基类。

---
+++ src/Tfx.Extensions.Hosting.ConfigurationManager/OperationSystemManagedHostEnvironment.cs
@@ -0,0 +1,40 @@
+﻿// © 2024 WANG YUCAI. LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
+
+using Niacomsoft.Utilities;
+
+namespace Niacomsoft.TeamFramework.Extensions.Hosting
+{
+    /// <summary> 提供了从操作系统环境变量获取 <c> .NET </c> 应用程序托管主机环境信息相关的 <see langword="abstract" /> 方法。 </summary>
+    /// <seealso cref="IManagedHostEnvironment" />
+    /// <seealso cref="ManagedHostEnvironment" />
+    public abstract class OperationSystemManagedHostEnvironment : ManagedHostEnvironment
+    {
+        /// <summary> 系统环境变量名称。 </summary>
+        public const string EnvironmentVariableName = "DOTNET_ENVIRONMENT";
+
+        /// <summary> 备选的环境变量名称。 </summary>
+        public const string SecondaryEnvironmentVariableName = "ASPNET_ENVIRONMENT";
+
+        /// <summary> 初始化 <see cref="OperationSystemManagedHostEnvironment" /> 类的新实例。 </summary>
+        /// <param name="name"> 托管主机环境名称。 </param>
+        protected OperationSystemManagedHostEnvironment(string name) : base(name)
+        {
+        }
+
+        /// <summary> 尝试从获取环境中获取托管主机环境信息 ( <see cref="EnvironmentVariableName" /> 或者 <see cref="SecondaryEnvironmentVariableName" />)。 </summary>
+        /// <param name="envVariableName"> 环境变量名称。 </param>
+        /// <param name="value">
+        /// 名称为 <see cref="EnvironmentVariableName" /> 或 <see cref="SecondaryEnvironmentVariableName" /> 的环境变量值。
+        /// </param>
+        /// <returns>
+        /// 当 <paramref name="value" /> 等于 <see langword="null" />、 <see cref="string.Empty" /> 或全为空白符时返回
+        /// <see langword="false" />；否则返回 <see langword="true" />。
+        /// </returns>
+        public static bool TryGetEnvironmentVariable(string envVariableName, out string value)
+        {
+            var envVariable = EnvironmentVariable.GetEnvironmentVariable(envVariableName);
+            value = StringUtilities.IfEmpty(envVariable?.Value, null, EmptyComparisonOptions.NullOrWhitespace);
+            return !AssertUtilities.IsEmpty(value, EmptyComparisonOptions.NullOrWhitespace);
+        }
+    }
+}
---
+++ src/Tfx.Extensions.Hosting.ConfigurationManager/README.md
@@ -0,0 +1,5 @@
+# README
+
+> This library provides a way to access environment information through System.Configuration for Microsoft .NET Framework only.
+> 
+> 此类库提供了仅适用于 “Microsoft .NET Framework”的，通过 System.Configuration 访问环境信息的方法
