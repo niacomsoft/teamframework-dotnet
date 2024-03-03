64f8b3053f3d364ae9b8c7c0edf11fb6db4e1fe9
Wang Yucai <netowls-studio@outlook.com>
2024-03-03 22:09:15 +0800

    feat(HostingEnvironment): 新增基于 .NET Hosting 访问托管主机环境信息的方法。

---
+++ src/Tfx.Extensions.Hosting/DotNetManagedHostEnvironment.cs
@@ -0,0 +1,34 @@
+﻿// © 2024 WANG YUCAI. LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
+
+using Microsoft.Extensions.Hosting;
+
+namespace Niacomsoft.TeamFramework.Extensions.Hosting
+{
+    /// <summary> 提供了基于 <c> Microsoft .NET </c> 平台 <see cref="IHostEnvironment" /> 访问托管主机环境相关的方法。 </summary>
+    /// <seealso cref="IHostEnvironment" />
+    /// <seealso cref="IManagedHostEnvironment" />
+    /// <seealso cref="ManagedHostEnvironment" />
+    public class DotNetManagedHostEnvironment : ManagedHostEnvironment
+    {
+        /// <summary> 初始化 <see cref="DotNetManagedHostEnvironment" /> 类的新实例。 </summary>
+        /// <param name="environment">
+        /// 访问 <c> .NET </c> 应用程序托管主机环境的方法。
+        /// <para> 实现了 <see cref="IHostEnvironment" /> 类型接口的对象实例。 </para>
+        /// </param>
+        public DotNetManagedHostEnvironment(IHostEnvironment environment) : this(environment?.EnvironmentName)
+        {
+            HostEnvironment = environment;
+        }
+
+        /// <summary> 初始化 <see cref="DotNetManagedHostEnvironment" /> 类的新实例。 </summary>
+        /// <param name="name"> 托管主机环境名称。 </param>
+        protected DotNetManagedHostEnvironment(string name) : base(name)
+        {
+        }
+
+        /// <summary> 访问 <c> .NET </c> 应用程序托管主机环境的方法。 </summary>
+        /// <value> 获取 <see cref="IHostEnvironment" /> 类型的对象实例，用于表示访问 <c> .NET </c> 应用程序托管主机环境的方法。 </value>
+        /// <seealso cref="IHostEnvironment" />
+        protected virtual IHostEnvironment HostEnvironment { get; }
+    }
+}
---
+++ src/Tfx.Extensions.Hosting/DotNetManagedHostEnvironmentExtensions.cs
@@ -0,0 +1,21 @@
+﻿// © 2024 WANG YUCAI. LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Niacomsoft.TeamFramework.Extensions.Hosting
+{
+    /// <summary> 为 <see cref="DotNetManagedHostEnvironment" /> 类型提供的扩展方法。 </summary>
+    public static class DotNetManagedHostEnvironmentExtensions
+    {
+        /// <summary> 注册访问 <c> .NET </c> 应用程序托管主机环境信息的方法。 </summary>
+        /// <param name="this"> 实现了 <see cref="IServiceCollection" /> 类型接口的对象实例。 </param>
+        /// <returns> 实现了 <see cref="IServiceCollection" /> 类型接口的对象实例 <paramref name="this" />。 </returns>
+        /// <seealso cref="DotNetManagedHostEnvironment" />
+        /// <seealso cref="IManagedHostEnvironment" />
+        /// <seealso cref="ManagedHostEnvironment" />
+        public static IServiceCollection AddManagedHostEnvironment(this IServiceCollection @this)
+        {
+            return @this.AddSingleton<IManagedHostEnvironment, DotNetManagedHostEnvironment>();
+        }
+    }
+}
