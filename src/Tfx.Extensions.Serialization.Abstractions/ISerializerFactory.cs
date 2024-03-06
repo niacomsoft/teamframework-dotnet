e950ded5da6a6aefc9c0069535fa2584b12b144e
Wang Yucai <netowls-studio@outlook.com>
2024-03-06 22:27:44 +0800

    feat(Serialization): 新增 ISerializer 相关工厂方法接口和抽象类。
    Issues #18

---
+++ src/Tfx.Extensions.Serialization.Abstractions/ISerializerFactory.cs
@@ -0,0 +1,13 @@
+﻿// © 2024 WANG YUCAI. LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
+
+namespace Niacomsoft.TeamFramework.Extensions.Serialization
+{
+    /// <summary> 定义了创建数据契约序列化程序 <see cref="ISerializer" /> 的工厂方法接口。 </summary>
+    public interface ISerializerFactory
+    {
+        /// <summary> 创建数据契约序列化程序。 </summary>
+        /// <returns> 实现了 <see cref="ISerializer" /> 类型接口的对象实例。 </returns>
+        /// <seealso cref="ISerializer" />
+        ISerializer CreateSerializer();
+    }
+}
---
+++ src/Tfx.Extensions.Serialization.Abstractions/SerializerFactory.cs
@@ -0,0 +1,42 @@
+﻿// © 2024 WANG YUCAI. LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
+
+using Niacomsoft.TeamFramework.Extensions.Logging;
+
+namespace Niacomsoft.TeamFramework.Extensions.Serialization
+{
+    /// <summary> 提供了创建数据契约序列化程序相关的 <see langword="abstract" /> 工厂方法。 </summary>
+    /// <seealso cref="ISerializerFactory" />
+    public abstract class SerializerFactory : ISerializerFactory
+    {
+        /// <summary> 初始化 <see cref="SerializerFactory" /> 类的新实例。 </summary>
+        /// <param name="logFactory">
+        /// 创建运行时日志的工厂方法。
+        /// <para> 实现了 <see cref="ILogWriterFactory" /> 类型接口的对象实例。 </para>
+        /// </param>
+        protected SerializerFactory(ILogWriterFactory logFactory)
+        {
+            LogFactory = logFactory;
+        }
+
+        /// <summary> 创建运行时日志的工厂方法。 </summary>
+        /// <value> 获取 <see cref="ILogWriterFactory" /> 类型的对象实例，用于表示创建运行时日志的工厂方法。 </value>
+        /// <seealso cref="ILogWriter" />
+        protected virtual ILogWriterFactory LogFactory { get; }
+
+        /// <inheritdoc />
+        public virtual ISerializer CreateSerializer()
+        {
+            return InternalCreateSerializer(LogFactory.CreateWriter());
+        }
+
+        /// <summary> 创建数据契约序列化程序。 </summary>
+        /// <param name="logWriter">
+        /// 记录运行时日志的方法。
+        /// <para> 实现了 <see cref="ISerializer" /> 类型接口的对象实例。 </para>
+        /// </param>
+        /// <returns> 实现了 <see cref="ISerializer" /> 类型接口的对象实例。 </returns>
+        /// <seealso cref="ILogWriter" />
+        /// <seealso cref="ISerializer" />
+        protected abstract ISerializer InternalCreateSerializer(ILogWriter logWriter);
+    }
+}
